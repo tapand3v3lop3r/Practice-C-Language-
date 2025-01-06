d92b4c5bd7cf04222ef9b2152ca3b89b86d3f89f
Tapan Kumar Padhi <164610686+tapand3v3lop3r@users.noreply.github.com>
2025-01-07 02:55:23 +0530

    Create 01_problem.c

---
+++ 01_problem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+      {
+            int a, b;
+            printf("This program will help you find out the area of a rectangle: \n");
+            printf("enter the length of the rectangle: \n");
+            scanf("%d", &a);
+            printf("enter the width of the rectangle: \n");
+            scanf("%d", &b);
+            printf("The area of the rectangle is: %d", a * b);
+            return 0;
+      }
